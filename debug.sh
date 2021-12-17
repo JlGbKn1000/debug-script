ac22f760f27aea195eb1c9c2b3fa2667bae18131
BuildTools <unconfigured@null.spigotmc.org>
2021-12-18 08:44:34 +0900

    Korean version

--- debug.sh
+++ debug.sh
@@ -1,12 +1,12 @@
 rm -r server
 mkdir server
-echo "Enter minecraft version: "
+echo "마인크래프트 버전: "
 read VERSION
 LATEST_BUILD=$(curl -X GET "https://papermc.io/api/v2/projects/paper/versions/$VERSION" -H  "accept: application/json" | jq '.builds[-1]')
 wget -O "server/paper.jar" "https://papermc.io/api/v2/projects/paper/versions/$VERSION/builds/$LATEST_BUILD/downloads/paper-$VERSION-$LATEST_BUILD.jar"
-wget -O "server/start.sh" "https://raw.githubusercontent.com/JlGbKn1000/debug-script/master/start.sh"
-echo "RAM: 4G"
-echo "Version: $VERSION"
-echo "Start server"
+wget -O "server/start.sh" "https://raw.githubusercontent.com/JlGbKn1000/debug-script/main/start.sh"
+echo "램: 4G"
+echo "버전: $VERSION"
+echo "서버를 시작합니다.."
 cd server
 ./start.sh
--- start.sh
+++ start.sh
@@ -1,12 +1,2 @@
-rm -r server
-mkdir server
-echo "Enter minecraft version: "
-read VERSION
-LATEST_BUILD=$(curl -X GET "https://papermc.io/api/v2/projects/paper/versions/$VERSION" -H  "accept: application/json" | jq '.builds[-1]')
-wget -O "server/paper.jar" "https://papermc.io/api/v2/projects/paper/versions/$VERSION/builds/$LATEST_BUILD/downloads/paper-$VERSION-$LATEST_BUILD.jar"
-wget -O "server/start.sh" "https://raw.githubusercontent.com/JlGbKn1000/debug-script/master/start.sh"
-echo "RAM: 4G"
-echo "Version: $VERSION"
-echo "Start server"
-cd server
-./start.sh
+java -Xms1024M -Xmx4096M -jar paper.jar nogui
+echo "서버가 종료되었습니다"
