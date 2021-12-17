3a8d1fcd1fae81ae9115c15f47ddb831789e48bd
BuildTools <unconfigured@null.spigotmc.org>
2021-12-18 08:51:24 +0900

    select build version

--- debug.sh
+++ debug.sh
@@ -2,11 +2,14 @@ rm -r server
 mkdir server
 echo "마인크래프트 버전: "
 read VERSION
-LATEST_BUILD=$(curl -X GET "https://papermc.io/api/v2/projects/paper/versions/$VERSION" -H  "accept: application/json" | jq '.builds[-1]')
+curl -X GET "https://papermc.io/api/v2/projects/paper/versions/$VERSION" -H  "accept: application/json"
+echo "빌드 버전을 선택하세요: "
+read LATEST_BUILD
 wget -O "server/paper.jar" "https://papermc.io/api/v2/projects/paper/versions/$VERSION/builds/$LATEST_BUILD/downloads/paper-$VERSION-$LATEST_BUILD.jar"
 wget -O "server/start.sh" "https://raw.githubusercontent.com/JlGbKn1000/debug-script/main/start.sh"
 echo "램: 4G"
 echo "버전: $VERSION"
+echo "빌드: $LATEST_BUILD"
 echo "서버를 시작합니다.."
 cd server
 ./start.sh
