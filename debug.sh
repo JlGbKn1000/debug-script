e4567fefde99e11d36f939516edc1542685ab34a
JlGbKn1000 <91351462+JlGbKn1000@users.noreply.github.com>
2021-12-18 12:54:51 +0900

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -3,20 +3,20 @@ mkdir server
 echo "#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).
 #Mon Mar 20 21:15:37 PDT 2017
 eula=true" > "server/eula.txt"
-echo "마인크래프트 버전: "
+echo "enter minecraft version: "
 read VERSION
 curl -X GET "https://papermc.io/api/v2/projects/paper/versions/$VERSION" -H  "accept: application/json"
-echo "\n빌드 버전을 선택하세요: "
+echo "\nselect build version: "
 read LATEST_BUILD
-echo "빌드한 파일을 선택하세요 (./build/$pluginName)"
+echo "select build folder (./build/$pluginName):"
 read BUILD_FILE
 wget -O "server/paper.jar" "https://papermc.io/api/v2/projects/paper/versions/$VERSION/builds/$LATEST_BUILD/downloads/paper-$VERSION-$LATEST_BUILD.jar"
 wget -O "server/start.sh" "https://raw.githubusercontent.com/JlGbKn1000/debug-script/main/start.sh"
 mkdir server/plugins
 mv"$BUILD_FILE" "server/plugins/"
-echo "램: 4G"
-echo "버전: $VERSION"
-echo "빌드: $LATEST_BUILD"
-echo "서버를 시작합니다.."
+echo "ram: 4G"
+echo "version: $VERSION"
+echo "build: $LATEST_BUILD"
+echo "starting server.."
 cd server
 ./start.sh
