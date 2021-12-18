cba0c8f79a4169b2bffa671eba0583959d0ef04b
BuildTools <unconfigured@null.spigotmc.org>
2021-12-18 09:05:00 +0900

    change files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 ## Debug-script
 ### 디버그를 위한 마인크래프트 서버 스크립트
+
+---
+
+* #### 사용법
+ * `wget -O "debug.sh" "https://raw.githubusercontent.com/JlGbKn1000/debug-script/main/debug.sh"`로 debug.sh를 다운로드 받는다 (wsl용)
+ * `./debug.sh`로 서버를 실행시킵니다
+
+---
--- debug.sh
+++ debug.sh
@@ -6,10 +6,14 @@ eula=true" => "server/eula.txt"
 echo "마인크래프트 버전: "
 read VERSION
 curl -X GET "https://papermc.io/api/v2/projects/paper/versions/$VERSION" -H  "accept: application/json"
-echo "빌드 버전을 선택하세요: "
+echo "\n빌드 버전을 선택하세요: "
 read LATEST_BUILD
+echo "빌드한 파일을 선택하세요"
+read BUILD_FILE
 wget -O "server/paper.jar" "https://papermc.io/api/v2/projects/paper/versions/$VERSION/builds/$LATEST_BUILD/downloads/paper-$VERSION-$LATEST_BUILD.jar"
 wget -O "server/start.sh" "https://raw.githubusercontent.com/JlGbKn1000/debug-script/main/start.sh"
+mkdir server/plugins
+mv"$BUILD_FILE" "server/plugins/"
 echo "램: 4G"
 echo "버전: $VERSION"
 echo "빌드: $LATEST_BUILD"
