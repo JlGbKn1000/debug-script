7e741641fe048b337a7d266e3f99a1854c59b578
JlGbKn1000 <91351462+JlGbKn1000@users.noreply.github.com>
2021-12-18 13:44:43 +0900

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -13,7 +13,7 @@ read BUILD_FILE
 wget -O "server/paper.jar" "https://papermc.io/api/v2/projects/paper/versions/$VERSION/builds/$LATEST_BUILD/downloads/paper-$VERSION-$LATEST_BUILD.jar"
 wget -O "server/start.sh" "https://raw.githubusercontent.com/JlGbKn1000/debug-script/main/start.sh"
 mkdir server/plugins
-mv"$BUILD_FILE" "server/plugins/"
+mv "$BUILD_FILE" "server/plugins/"
 echo "ram: 4G"
 echo "version: $VERSION"
 echo "build: $LATEST_BUILD"
