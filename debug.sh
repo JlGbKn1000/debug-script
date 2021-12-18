198252dd394bab3a8590df392db5d87dfe891710
JlGbKn1000 <91351462+JlGbKn1000@users.noreply.github.com>
2021-12-18 09:09:03 +0900

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -2,13 +2,13 @@ rm -rf server
 mkdir server
 echo "#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).
 #Mon Mar 20 21:15:37 PDT 2017
-eula=true" => "server/eula.txt"
+eula=true" > "server/eula.txt"
 echo "마인크래프트 버전: "
 read VERSION
 curl -X GET "https://papermc.io/api/v2/projects/paper/versions/$VERSION" -H  "accept: application/json"
 echo "\n빌드 버전을 선택하세요: "
 read LATEST_BUILD
-echo "빌드한 파일을 선택하세요"
+echo "빌드한 파일을 선택하세요 (./build/$pluginName)"
 read BUILD_FILE
 wget -O "server/paper.jar" "https://papermc.io/api/v2/projects/paper/versions/$VERSION/builds/$LATEST_BUILD/downloads/paper-$VERSION-$LATEST_BUILD.jar"
 wget -O "server/start.sh" "https://raw.githubusercontent.com/JlGbKn1000/debug-script/main/start.sh"
